f14e1b8cf469f3e446296b66a4130ad37bb24877
Will Keeble <will.a.keeble@gmail.com>
2024-10-31 22:54:10 +1100

    add player setup

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from constants import *
 import pygame
+from player import Player
+from constants import *
 
 
 def main():
     # initialise pygame and window
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     # track FPS
     clock = pygame.time.Clock()
-    dt = 0 # time change (sec)
+    dt = 0  # time change (sec)
 
     # main game loop
     while True:
@@ -17,9 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0)) # black screen
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000 # update time
+
+        # limit framerate to 60 fps
+        dt = clock.tick(60) / 1000  # update time
 
 
 if __name__ == "__main__":
