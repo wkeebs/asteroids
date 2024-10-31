2b6638ba3b816086d6e991e4ee449d8a95df877f
Will Keeble <will.a.keeble@gmail.com>
2024-10-31 22:27:53 +1100

    add initial game constants

---
+++ main.py
@@ -0,0 +1,9 @@
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
