99ae15d360ac52dbc32bacedd12c0588fedc7853
Will Keeble <will.a.keeble@gmail.com>
2024-11-01 01:53:42 +1100

    add movement momentum

--- player.py
+++ player.py
@@ -4,12 +4,17 @@ from constants import *
 from circleshape import CircleShape
 from shot import Shot
 
+def smooth(x):
+    """ Smoothing function for momentum calculation """
+    return (x * 3) ** 2 / 3
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.__shot_timer = 0
+        self.__momentum_timer = 0
+        self.__momentum_direction = pygame.Vector2(0, 0)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,9 +32,13 @@ class Player(CircleShape):
     @override
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        
-        self.__shot_timer -= dt
 
+        self.__shot_timer -= dt
+        self.__momentum_timer = max(self.__momentum_timer - dt, 0)
+
+        # momentum movement
+        self.position += self.__momentum_direction * smooth(self.__momentum_timer)
+            
         if keys[pygame.K_a]:
             self.rotate(dt)
         if keys[pygame.K_d]:
@@ -47,13 +56,16 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-    
+        direction = 1 if dt > 0 else -1 # forward/backwards momentum
+        self.__momentum_direction = pygame.Vector2(0, direction).rotate(self.rotation)
+        self.__momentum_timer = 1
+
     def shoot(self):
         if self.__shot_timer > 0:
             # rate limiting
             return
-        
+
         shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        shot.velocity = pygame.Vector2(0, 1).rotate(
+            self.rotation) * PLAYER_SHOOT_SPEED
         self.__shot_timer = PLAYER_SHOOT_COOLDOWN
-        
