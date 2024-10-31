a275e9b69848ad33b95f678a2986bac75c5ef52a
Will Keeble <will.a.keeble@gmail.com>
2024-10-31 22:39:00 +1100

    setup basic window

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from constants import *
+import pygame
+
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
